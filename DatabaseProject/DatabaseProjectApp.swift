34b6bee7abc77c6873a8ffadd1afc8aab8922a90
ihaque94 <ihaque94@cs.ubc.ca>
2023-09-04 01:13:07 -0700

    Initial Commit

---
+++ DatabaseProject/DatabaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseProjectApp.swift
+//  DatabaseProject
+//
+//  Created by Ishita Haque on 2023-09-04.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
