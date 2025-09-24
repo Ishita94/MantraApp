6d374234e56491584bc6fd99cee1d11e31fa6932
Ishita94 <haqueishita@gmail.com>
2025-09-24 12:52:46 -0500

    More pages initialized

---
+++ DatabaseProject/View/More/Reminders/ReminderRow.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderRow.swift
+//  DatabaseProject
+//
+//  Created by Ishita Haque on 2025-09-24.
+//
+
+import SwiftUI
+
+struct ReminderRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderRow()
+}
---
+++ DatabaseProject/View/More/Reminders/RemindersLandingPage.swift
@@ -0,0 +1,18 @@
+//
+//  RemindersLandingPage.swift
+//  DatabaseProject
+//
+//  Created by Ishita Haque on 2025-09-24.
+//
+
+import SwiftUI
+
+struct RemindersLandingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemindersLandingPage()
+}
